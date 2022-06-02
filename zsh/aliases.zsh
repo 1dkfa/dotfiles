c8f2c6bed3ede397b36b84f11d675d08fc62baaf
1dkfa <37644682+1dkfa@users.noreply.github.com>
2022-06-02 08:03:36 +0300

    add zsh aliases & keymaps

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,15 +1,15 @@
+# filesystem
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+
 # helpers
 alias grep='grep --color=auto'
 alias egrep='egrep --color=auto'
 alias fgrep='fgrep --color=auto'
 alias df='df -h' # disk free, in GB
 alias du='du -h -c' # calculate disk usage for a directory
-
-# filesystem
-alias ..='cd ..'
-alias ...='cd ../..'
-alias ....="cd ../../.."
-alias .....="cd ../../../.."
 
 if ls --color > /dev/null 2>&1; then # detect which flavor of `ls` is in use
     colorflag="--color" # GNU `ls`
@@ -57,3 +57,5 @@ alias glg='git log --graph --abbrev-commit --decorate --format=format:'\''%C(bol
 alias mvwp='mv ~/Downloads/*.png ~/iCloud/Pictures/wallpapers ; echo "All .PNG files from ~/Downloads moved to ~/iCloud/Pictures/wallpapers"'
 alias caff='caffeinate -disu &; caff=$!; echo "caffeinate'\''s PID ($caff) has been declared to variable \$caff"'
 alias tsm='transmission-remote'
+alias hidedesktop="defaults write com.apple.finder CreateDesktop -bool false && killall Finder"
+alias showdesktop="defaults write com.apple.finder CreateDesktop -bool true && killall Finder"
--- zsh/keymaps.zsh
+++ zsh/keymaps.zsh
@@ -1,4 +1,19 @@
+# movement
+bindkey '^A' beginning-of-line                      # [Ctrl-a] - move to the beginning of the line
+bindkey '^E' end-of-line                            # [Ctrl-e] - move to the end of the line
+bindkey '^F' forward-word                           # [Ctrl-f] - move forward one word
+bindkey '^B' backward-word                          # [Ctrl-b] - move backward one word
+
 bindkey '^[[Z' reverse-menu-complete                # [Shift-Tab] - go backwards in complete menu
+
+bindkey "^N" history-beginning-search-forward       # [Ctrl+n] - forward zsh history
+bindkey "^J" history-beginning-search-forward       # [Ctrl-j] - forward zsh history
+bindkey "^K" history-beginning-search-backward      # [Ctrl-k] - backward zsh history
+bindkey "^P" history-beginning-search-backward      # [Ctrl-p] - backward zsh history
+
+# modifying text
+bindkey "^W" backward-kill-word                     # [Ctrl+w] - kill the line behind cursor
+bindkey "^U" kill-whole-line                        # [Ctrl-u] - kill whole line
 bindkey '^?' backward-delete-char                   # [Backspace] - delete backward
 if [[ "${terminfo[kdch1]}" != "" ]]; then
     bindkey "${terminfo[kdch1]}" delete-char        # [Delete] - delete forward
@@ -7,5 +22,3 @@ else
     bindkey "^[3;5~" delete-char
     bindkey "\e[3~" delete-char
 fi
-bindkey "^J" history-beginning-search-forward       # [Ctrl-j] - forward zsh history
-bindkey "^K" history-beginning-search-backward      # [Ctrl-k] - backward zsh history
