b1c15ca6fbcba6e694679b57105d3d017f685ce6
mkd <mkd@medos.io>
2022-06-02 08:25:45 +0300

    add alpha & impatient plugins to nvim

--- config/nvim/lua/user/plugins.lua
+++ config/nvim/lua/user/plugins.lua
@@ -52,6 +52,8 @@ return packer.startup(function(use)
 	use "nvim-lualine/lualine.nvim"
 	use "moll/vim-bbye"
 	use "akinsho/toggleterm.nvim"
+	use "lewis6991/impatient.nvim"
+	use "goolord/alpha-nvim"
 
 	-- Colorschemes
 	-- use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
